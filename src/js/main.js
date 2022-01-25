1b9bb7f1e802e2e540d1e9e3b5363d4d571d585d
Adrianrjs <adrianrojass86@gmail.com>
2022-01-25 18:02:06 -0300

    consume music and images

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,24 @@ const songList = [
     {
         title: "Soft Skin",
         file:"Soft Skin.mp3",
-        cover:"cat-dj.jpg"
+        cover:"catsad-1.jpg"
     },
     {
         title: "sagun - I'll Keep You Safe (feat. Shiloh)",
         file:"sagun - I'll Keep You Safe (feat. Shiloh).mp3",
-        cover:"cat-dj.jpg"
+        cover:"catsad-2.jpg"
     },
     {
         title: "timmies - tell me why i'm waiting (ft. shiloh)",
         file:"timmies - tell me why i'm waiting (ft. shiloh).mp3",
-        cover:"cat-dj.jpg"
+        cover:"catsad-3.jpg"
     },
 ]
 
 //Capturar elementos del DOM para utilizarlo
 const songs = document.getElementById("songs")
+const audio = document.getElementById("audio")
+const cover = document.getElementById("cover")
 //Cargar canciones y mostrar listado
 function loadSongs() {
     songList.forEach((song, index) => {
@@ -25,11 +27,11 @@ function loadSongs() {
         const li = document.createElement("li")
         // Crear a
         const link = document.createElement("a")
-        // Hidrtar a
+        // Hidartar a
         link.textContent = song.title
         link.href = "#"
         //Escuchar clicks
-        link.addEventListener("click", () => loadSong(index)  )
+        link.addEventListener("click", () => loadSong(index))
         // Añadir a li
         li.appendChild(link)
         // Añadir li a ul
@@ -39,7 +41,10 @@ function loadSongs() {
 
 // Cargar cancion seleccionada
 function loadSong(songIndex) {
-    console.log(songIndex)
+    audio.src = "src/audio/" + songList[songIndex].file
+    audio.play()
+
+    cover.src = "src/images/" + songList[songIndex].cover
 }
 
 //Go!
